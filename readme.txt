85202f35c8347a67b385708cbc685c1dff5c7d36
egotec <egotec@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-25 08:01:43 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/viewmybrowser/trunk@548872 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== ViewMyBrowser ===
+Contributors: egotec
+Donate link: http://www.viewmybrowser.com/
+Tags: widget, plugin, admin, sidebar, screen, sharing, remote, control, browser, support, customer, advisor, meeting
+Requires at least: 3
+Tested up to: 3.5
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Enhance your WordPress with remote support. ViewMyBrowser is a screen sharing, browser sharing, remote access solution for web applications.
+
+== Description ==
+
+ViewMyBrowser is the leading browser sharing solution for WordPress.
+This plugin activates the awesome features of a remote support and co-browsing solution on your WordPress installation.
+
+Features:
+* Your customers get instant online support without installing any software or browser plugins.
+* Support staff can view the browser content of their customers in real time.
+* Remote Control: Show editors how to use your wordpress installation.
+* Enable remote support for your frontend users. No more questioning which button to click. Support staff can show the way you do it right directly in an online meeting.
+
+== Installation ==
+
+1. Extract viewmybrowser.zip to `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+Or just use the wordpress plugins dialog.
+
+== Frequently Asked Questions ==
+
+= Where do I get my Site-ID from? =
+Sign up to ViewMyBrowser at [www.viewmybrowser.com](http://www.viewmybrowser.com). Open the administration dialog, switch to the sites tab,
+click on a site and copy the unique Site-ID.
+
+= Where do I find more information? =
+Go to [help.viewmybrowser.com](http://help.viewmybrowser.com).
+
+== Screenshots ==
+
+1. ViewMyBrowser support dialog in backend.
+2. ViewMyBrowser support dialog in frontend.
+
+== Changelog ==
+
+= 1.0 =
+* First stable release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+Get remote support for your customers.
---
+++ viewmybrowser.php
@@ -0,0 +1,268 @@
+<?php
+/*
+Plugin Name: ViewMyBrowser Support
+Plugin URI: http://www.viewmybrowser.com/
+Description: Zur einbindung der ViewMyBrowser Supportlösung in ihre Wordpress Homepage
+Version: 1.0
+Author: ViewMyBrowser
+Author URI: http://www.viewmybrowser.com/
+Update Server: http://viewmybrowser.appspot.com/plugins/wordpress/viewmybrowser/
+*/
+
+/**
+ * ViewMyBrowser plugin für Wordpress
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Verbatim copying and distribution of this entire article is permitted in any medium without royalty provided this notice is preserved.
+ *
+ * @link http://www.viewmybrowser.com
+ * @version 1.0
+ * @copyright Copyright: 2012 EGOTEC GmbH
+ * @author EGOTEC GmbH <sales@viewmybrowser.com>
+ */
+
+error_reporting(0);
+
+add_action('widgets_init', array('ViewMyBrowser', 'init'));
+class ViewMyBrowser {
+	//Variabelen Setzen
+	public static $siteID;
+	public static $clientID;
+	public static $current_user;
+	public static $showInFrontEnd;
+	public static $showInBackEnd;
+	public static $updated = false;
+	public static $plugin_url;
+
+	public static function init() {
+		if ($_POST['action'] == 'update') {
+			self::update();
+		}
+		self::$current_user = wp_get_current_user();
+		self::$siteID = self::getSiteID();
+		self::$showInFrontEnd = self::getShowInFrontEnd();
+		self::$showInBackEnd = self::getShowInBackEnd();
+		self::$plugin_url =  plugin_dir_url() . "viewmybrowser/";
+		
+		if(isset(self::$current_user->display_name)) {
+			self::$clientID = self::$current_user->display_name;
+		} elseif(isset(self::$current_user->user_email)) {
+			self::$clientID = self::$current_user->user_email;
+		} elseif(isset(self::$current_user->user_login)) {
+			self::$clientID = self::$current_user->user_login;
+		} else {
+			self::$clientID = __("Guest");
+		}
+
+		if(is_admin()) {
+			self::createAdminMenu();
+		} else {
+			self::showViewMyBrowserUI();
+		}
+	}
+
+	public static function update() {
+		self::updateSiteID($_POST['siteID']);
+		self::updateShowInFrontEnd(isset($_POST['showInVMBFrontend']));
+		self::updateShowInBackEnd(isset($_POST['showInVMBBackend']));
+		self::$updated = true;
+	}
+
+	//SiteID
+	public static function getSiteID() {
+		return get_option('siteID');
+	}
+
+	public static function updateSiteID($siteID) {
+		update_option('siteID', $siteID);
+	}
+	
+	//Show in Frontend
+	public static function getShowInFrontEnd() {
+		return get_option('showInVMBFrontend');
+	}
+	
+	public static function updateShowInFrontEnd($stat) {
+		update_option('showInVMBFrontend', $stat);
+	}
+	
+	//Show in Backend
+	public static function getShowInBackEnd() {
+		return get_option('showInVMBBackend');
+	}
+	
+	public static function updateShowInBackEnd($stat) {
+		update_option('showInVMBBackend', $stat);
+	}
+	
+	public static function createAdminMenu() {
+		add_action('admin_menu', array('ViewMyBrowser', 'viewmybrowser_menu_create'));
+	}
+
+	public static function showViewMyBrowserUI() {
+		if(self::$showInFrontEnd && self::$siteID != "") {
+			add_action('wp_head', array('ViewMyBrowser', 'viewmybrowser_add_label'));
+		}
+	}
+
+	public static function getCodeSnippet($hidden) {
+		return "<script type=\"text/javascript\" charset=\"utf-8\"> document.write(unescape('%3Cscript type=\"text/javascript\"".
+			" charset=\"utf-8\" src=\"' + document.location.protocol + '//viewmybrowser.appspot.com/client.js\"" . ($hidden == true ? "hidden=\"true\"" : '') . " id=\"viewmybrowser\"".
+			" siteID=\"" . htmlspecialchars(self::$siteID) . "\" clientID=\"".htmlspecialchars(self::$clientID)."\" cacheDom=\"true\" docType=\'\'%3E%3C/script%3E'));</script>";
+	}
+
+	/* Frontend */
+
+	//Einbinden der Augen
+	public function viewmybrowser_add_label($content) {
+		echo ViewMyBrowser::getCodeSnippet(false);
+	}
+
+	/* Admin Panel */
+
+	//Init-AdminPanel
+	public function viewmybrowser_menu_create() {
+		if (ViewMyBrowser::$showInBackEnd && self::$siteID != "") {
+			add_action('admin_head', array('ViewMyBrowser', 'viewmybrowser_add_label'));
+		}
+		add_menu_page(
+			'ViewMyBrowser',
+			'ViewMyBrowser', 'administrator',
+			__FILE__,
+			array('ViewMyBrowser', 'viewmybrowser_settings_page'),
+			plugins_url('vmb_ico.png', __FILE__)
+		);
+		if (self::$showInBackEnd) {
+			add_submenu_page(__FILE__, 
+					'',
+					'Live Support',
+					'manage_options',
+					'javascript:viewMyBrowser.show();'
+			);
+		}
+	}
+
+	public function viewmybrowser_settings_page() {
+		if (self::$updated) {
+			echo "<div class='updated'> \n";
+			echo "<p><strong>".__('Settings saved.')."</strong></p>";
+			echo "</div> \n";
+		}
+		if (self::$siteID == "") {
+			echo "<div class='updated'> \n";
+			echo "<p><strong>".VMBTranslate::translate('missingSiteID')."</strong></p>";
+			echo "</div> \n";
+		}
+		print '
+		<div class="wrap">
+			<style type="text/css">
+				td {
+					padding: 5px;
+				}
+				
+				table {
+					color: #464646;
+					font-size: 1.1em;
+					font-family: arial, verdana, sans-serif;
+				}
+			</style>
+			<div id="icon-options-general" class="icon32">
+				<br>
+			</div>
+				<h2>ViewMyBrowser '.VMBTranslate::translate('settings').'</h2>
+				<br />
+				<form action="" method="POST"  style="border: 1px solid #999999; padding: 5px;">
+					<table>
+						<tr>
+							<td>Site-ID:</td>
+							<td><input type="text" name="siteID" value="'.htmlspecialchars(self::getSiteID()).'" style="width: 500px;"/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td>'.VMBTranslate::translate('getSiteID').'</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><a href="http://www.viewmybrowser.com/#signup" target="_blank">' . VMBTranslate::translate('here') . '</a> ' . VMBTranslate::translate('create') . '</td>
+						</tr>
+						<tr>
+							<td><input type="checkbox" name="showInVMBBackend" value="1"'.(self::$showInBackEnd ? ' checked="checked"' : '').' /></td>
+							<td><img src="' . self::$plugin_url . 'img/backend.png"></img></td>
+							<td>'.VMBTranslate::translate('backend').'</td>
+						</tr>
+						<tr>
+							<td><input type="checkbox" name="showInVMBFrontend" value="1"'.(self::$showInFrontEnd ? ' checked="checked"' : '').' /></td>
+							<td><img src="' . self::$plugin_url . 'img/frontend.png"></img></td>
+							<td>'.VMBTranslate::translate('frontend').'</td>
+						</tr>
+					</table>
+					<p class="submit">
+						<input type="submit" class="button-primary" value="' . VMBTranslate::translate('update') . '" />
+						<input type="hidden" name="action" value="update" />
+					</p>
+				</form>
+		</div>';
+	}
+}
+
+class VMBTranslate {
+	public static $data = array(
+		'de' => array(
+						'update'=>'Aktualisiere ViewMyBrowser Einstellungen',
+						'backend'=>'ViewMyBrowser Widget im Adminbereich aktivieren',
+						'frontend'=>'ViewMyBrowser Widget im Blog aktivieren',
+						'create'=>'können sie ihren eigenen ViewMyBrowser Account erstellen',
+						'here'=>'Hier',
+						'getSiteID'=>'Woher bekomme ich eine ViewMyBrowser Site-ID?',
+						'settings'=>'Einstellungen',
+						'missingSiteID'=>'Keine Site-ID vorhanden. Bitte trangen sie eine Site-ID in den ViewMyBrowser Einstellungen ein!'
+						),
+		'en' => array(
+						'update'=>'Update ViewMyBrowser Settings',
+						'backend'=>'Enable ViewMyBrowser widget in Wordpress',
+						'frontend'=>'Enable ViewMyBrowser widget in Website.',
+						'create'=>'you can create your own ViewMyBrowser account.',
+						'here'=>'Here',
+						'getSiteID'=>'Where do I get my ViewMyBrowser Site-ID?',
+						'settings'=>'Settings',
+						'missingSiteID'=>'Missing ViewMyBrowser Site-ID. Please set your Site-ID in ViewMyBrowser Settings!'
+						)
+	);
+	
+	public static function translate($key) {
+		$lang = strtolower(substr((WPLANG != '' ? WPLANG : 'en'), 0, 2));
+		if ($lang != 'de') {
+			$lang = 'en';
+		}
+		$result = self::$data[$lang][$key];
+		return isset($result) ? $result : $key;
+	}
+}
+
+function ViewMyBrowser_install() {
+	add_option('siteID', '');
+	add_option('showInVMBFrontend', false);
+	add_option('showInVMBBackend', true);
+}
+
+function ViewMyBrowser_uninstall() {
+	delete_option('siteID');
+	delete_option('showInVMBFrontend');
+	delete_option('showInVMBBackend');
+}
+
+register_activation_hook(__FILE__, 'ViewMyBrowser_install');
+register_deactivation_hook(__FILE__, 'ViewMyBrowser_uninstall');
+?>
